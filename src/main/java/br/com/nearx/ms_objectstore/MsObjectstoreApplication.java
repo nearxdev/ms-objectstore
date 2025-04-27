b16523f934f7e7887d878fa3eed1acaffedcb381
romulospl <romulopereiralopes@gmail.com>
2025-04-27 22:30:17 +0000

    :tada: Commit inicial

---
+++ src/main/java/br/com/nearx/ms_objectstore/MsObjectstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.nearx.ms_objectstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsObjectstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsObjectstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nearx/ms_objectstore/MsObjectstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nearx.ms_objectstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsObjectstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
